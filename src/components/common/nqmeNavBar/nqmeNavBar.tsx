408a5b7e33d4c22e467d1a6ae2146a6d6fba4e01
SpasZahariev <spaszahariev54@gmail.com>
2019-05-30 08:18:41 +0100

    added a video box

--- src/components/common/nqmeNavBar/nqmeNavBar.tsx
+++ src/components/common/nqmeNavBar/nqmeNavBar.tsx
@@ -14,13 +14,6 @@ const NqmeNavBar: React.FC<Props> = props => {
     <div className="container-fluid nav-container">
       <div className="nqme-row">
         <h4 id="logo-header">NQME {props.textForUser}</h4>
-        {/* <h5 className="attention-message">{props.textForUser}</h5> */}
-        {/* <form className="form-inline" onSubmit={props.onSearchSong}>
-          <input className="form-control" placeholder="Search Song" />
-          <button className="btn btn-primary m-2" type="submit">
-            click
-          </button>
-        </form> */}
         <div className="input-group" id="search-div">
           <input
             type="text"
--- src/components/masterPage/masterPage.tsx
+++ src/components/masterPage/masterPage.tsx
@@ -3,36 +3,34 @@ import Room from "../common/room";
 import "./masterPage.scss";
 import "../common/nqmeNavBar/nqmeNavBar";
 import NqmeNavBar from "../common/nqmeNavBar/nqmeNavBar";
+import YouTube from "react-youtube";
 
 type Props = {
   room: Room;
+};
+
+const youtubeOptions = {
+  height: "500",
+  width: "100%",
+
+  PlayerVars: {
+    // https://developers.google.com/youtube/player_parameters
+    // autoplay: 1
+    color: "white",
+    iv_load_policy: 3
+  }
 };
 
 class MasterPage extends Component<Props> {
   render() {
     return (
       <div className="main-container col-lg-12 col-xl-9">
-        {/* <div className="container-fluid nav-container">
-          <span>
-            <h4>NQME</h4>
-          </span>
-        </div> */}
         <NqmeNavBar
           textForUser="Master"
           onSearchSong={this.onSearchSong}
           usersInRoom={2}
         />
-        <div className="container-fluid content-container">
-          <h1>more Stuff</h1>
-        </div>
-        {/* <div className="custom-nav">
-          <span>NQME</span>
-        </div>
-        <div className="container-fluid content-container">
-          <h1>You tube test machine</h1>
-          <h1>{this.props.room._id}</h1>
-          <img src="https://picsum.photos/500/900" alt="idk is this ok" />
-        </div> */}
+        <div className="content-container">{this.renderPlayer()}</div>
       </div>
     );
   }
@@ -41,6 +39,38 @@ class MasterPage extends Component<Props> {
     evt.preventDefault();
     // todo
     console.log("you serached a song!");
+  }
+
+  // the songs played part ensures that the player gets refreshed at the end of a song
+  renderPlayer() {
+    return (
+      <div id="music-player">
+        {/* {this.state.currentSong.type === "s" && spotifyApi.getAccessToken() && (
+          <SpotifyPlayer
+            spotifyToken={spotifyApi.getAccessToken()}
+            songUri={this.state.currentSong.url}
+            songName={this.state.currentSong.name}
+            next={this._onEnd}
+          />
+        )} */}
+
+        {/* {this.state.currentSong.type === "y" && (
+          <YouTube
+            videoId={this.state.currentSong.url}
+            opts={youtubeOptions}
+            onEnd={this._onEnd}
+          />
+        )} */}
+
+        <YouTube
+          // videoId={this.state.currentSong.url}
+          videoId="3kQXKJJ0nGc"
+          opts={youtubeOptions}
+          onReady={event => event.target.playVideo()}
+          // onEnd=({this._onEnd})
+        />
+      </div>
+    );
   }
 }
 
